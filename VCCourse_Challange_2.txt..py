204ca6c18db0a7a01149122beb54fa22fb206581
JuneArness <84601279+JuneArness@users.noreply.github.com>
2021-05-25 10:13:11 -0400

    Add files via upload

---
+++ VCCourse_Challange_2.txt..py
@@ -0,0 +1,46 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name= Stephen
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    name= Stephen
+NameError: name 'Stephen' is not defined
+>>> name_Stephen
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    name_Stephen
+NameError: name 'name_Stephen' is not defined
+>>> name_Stephen
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    name_Stephen
+NameError: name 'name_Stephen' is not defined
+>>> name
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    name
+NameError: name 'name' is not defined
+>>> name=("Stephen")
+>>> name
+'Stephen'
+>>> write("Hello World, my name is Stephen")
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    write("Hello World, my name is Stephen")
+NameError: name 'write' is not defined
+>>> print("Hello World, my name is Stephen")
+Hello World, my name is Stephen
+>>> 
+= RESTART: C:/Users/jburg/AppData/Local/Programs/Python/Python39/VCCourse_Challenge_1.txt..py
+Hello World, My name is Stephen
+>>> 
+==== RESTART: C:/Users/jburg/OneDrive/Documents/VCCourse_Challenge_2.txt..py ===
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> 
+==== RESTART: C:/Users/jburg/OneDrive/Documents/VCCourse_Challenge_2.txt..py ===
+Red is my favorite color
+>>> 
